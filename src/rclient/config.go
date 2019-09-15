d2a3a0d6486a609ff4a0ff804dac64e891ed780c
Marco Tulio R Braga <marco.tulio@mtulio.eng.br>
2019-09-15 10:52:58 -0300

    feat/ssl-disable: add option to disable SSL check

--- src/rclient/config.go
+++ src/rclient/config.go
@@ -7,11 +7,19 @@ type RConf struct {
 	Base       *rundeck.ClientConfig
 	EnableAPI  bool
 	EnableHTTP bool
+	verifySSL  bool
 }
 
 // NewConfig Returns configurator
 func NewConfig() (*RConf, error) {
 	return &RConf{
-		Base: &rundeck.ClientConfig{},
+		Base:      &rundeck.ClientConfig{},
+		verifySSL: true,
 	}, nil
+}
+
+// DisableVerifySSL disables the HTTP SSL check attribute.
+func (rc *RConf) DisableVerifySSL() bool {
+	rc.verifySSL = false
+	return true
 }
